983354377d67e73f550d7046d16fab7a4d517c21
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-03-31 19:22:22 +0500

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.courses__carousel_inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true
+    });
+});
